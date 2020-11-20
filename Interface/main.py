4c7153eb90c1d1b3bc05b564ec6e91c480b14f05
BrosephB <BrosephB@users.noreply.github.com>
2020-11-19 19:45:30 -0800

    background colour in main.py
    setting the blue colour that will be a part of the colour scheme of the app. This background is for when the user first starts the app and the colour "flashes" for effect

--- Interface/main.py
+++ Interface/main.py
@@ -1,7 +1,16 @@
 from kivy.app import App
+from kivy.core.window import Window
+
+# the arguments used here are R G B A (red,green,blue,alpha) 
+# sets background colour
+# each entry has to be between 0 and 1 so for example, 0.5 for green means 50% green
+
+Window.clearcolor = (0,0.5,1,1)
 
 class PhotoApp(App):
-    pass
+    
+    def build(self):
+        return 
 
 if __name__ == '__main__':
     PhotoApp().run()
