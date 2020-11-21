2279c88ef42681c0909799f77afd00bc794d7609
BrosephB <BrosephB@users.noreply.github.com>
2020-11-20 22:04:41 -0800

    Getting an initial gridlayout for the first screen

--- Interface/main.py
+++ Interface/main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.core.window import Window
 
+
 # the arguments used here are R G B A (red,green,blue,alpha) 
 # sets background colour
 # each entry has to be between 0 and 1 so for example, 0.5 for green means 50% green
@@ -8,9 +9,8 @@ from kivy.core.window import Window
 Window.clearcolor = (0.4,0.74,0.91,0.7)
 
 class PhotoApp(App):
-    
     def build(self):
-        return 
+        return
 
 if __name__ == '__main__':
     PhotoApp().run()
