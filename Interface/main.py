32b86f705d54fd9f7237b7bbe5f770dbdc40a7a3
BrosephB <BrosephB@users.noreply.github.com>
2020-11-22 20:35:14 -0800

    Updating style of Interface
    PhotoRoot has been added as well as a widget containing the inner white box as part of our colour scheme

--- Interface/main.py
+++ Interface/main.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
 
 
 # the arguments used here are R G B A (red,green,blue,alpha) 
@@ -7,6 +9,20 @@ from kivy.core.window import Window
 # each entry has to be between 0 and 1 so for example, 0.5 for green means 50% green
 
 Window.clearcolor = (0.4,0.74,0.91,0.7)
+
+
+
+class PhotoRoot(FloatLayout):
+    """Custom Class inheriting from floatlayout to provide no restriction
+    on its child widgets"""
+    
+    pass
+
+class Logo(Image):
+    """Our Custom class pertaining to our logo which inherits from the Image
+    widget"""
+    
+    pass 
 
 class PhotoApp(App):
     def build(self):
