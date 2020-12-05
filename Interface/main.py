f5c9a485d78cb6a1e91970759c8790aa25f8d472
BrosephB <BrosephB@users.noreply.github.com>
2020-12-04 17:42:51 -0800

    First rendition of GUI complete
    although the code can still be refactored, all logic that kivy could be given has been done and all screens are intact. A custom font has also been added

--- Interface/main.py
+++ Interface/main.py
@@ -4,13 +4,16 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
-
+from kivy.core.text import LabelBase
 
 # the arguments used here are R G B A (red,green,blue,alpha) 
 # sets background colour
 # each entry has to be between 0 and 1 so for example, 0.5 for green means 50% green
 
 Window.clearcolor = (0.4,0.74,0.91,0.7)
+Window.size = (375,667)
+
+
 
 class PhotoRoot(FloatLayout):
     """Custom Class inheriting from floatlayout to provide no restriction
@@ -33,6 +36,9 @@ class LoadingWindow(Screen):
 class PhotoDisplayWindow(Screen):
     pass
 
+class PhotoZoomWindow(Screen):
+    pass
+
 class WindowManager(ScreenManager):
     pass
 
@@ -41,4 +47,6 @@ class ImitariApp(App):
         return
 
 if __name__ == '__main__':
+    LabelBase.register(name= 'LemonMilk', fn_regular = 'Fonts/LemonMilkMedium.ttf',
+                   fn_bold = 'Fonts/LemonMilkBold.ttf')
     ImitariApp().run()
